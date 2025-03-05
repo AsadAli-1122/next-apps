8e9965df3f8280e3ccbd4d9ba5ebea82f32d2bc3
AsadAli-1122 <asadali.cool.23@gmail.com>
2025-03-05 19:40:15 +0500

    add main

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="">
+        <main>
         {children}
+        </main>
       </body>
     </html>
   );
